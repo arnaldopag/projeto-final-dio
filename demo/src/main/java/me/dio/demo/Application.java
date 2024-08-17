2c65e96af38abc0e1ff1c2eeb04b54e2bed30cd7
Arnaldo Pagani Junior <arnaldopaganijr@gmail.com>
2024-08-17 14:18:29 -0300

    ajutes para postar no railway

--- demo/src/main/java/me/dio/demo/Application.java
+++ demo/src/main/java/me/dio/demo/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
+	public String port = System.getenv("PORT");
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
